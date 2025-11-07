1e50db9db6f943610a5ca7378321a231fb1cc02c
Joseph Vigil <joeyvigil109329@gmail.com>
2025-11-06 17:04:54 -0700

    first commit

---
+++ flask_app.py
@@ -0,0 +1,10 @@
+from app.models import db
+from app import create_app
+
+app = create_app('DevelopmentConfig') # change to 'ProductionConfig' as needed
+
+with app.app_context():
+    # db.drop_all() 
+    db.create_all() 
+
+app.run() #remove when deploying to production server
