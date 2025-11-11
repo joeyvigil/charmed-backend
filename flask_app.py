51d99a0dabdd9de5f31fa8872835266e0f6fa6b9
Joseph Vigil <joeyvigil109329@gmail.com>
2025-11-10 17:55:42 -0700

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -4,7 +4,7 @@ from app import create_app
 app = create_app('ProductionConfig') # change to 'ProductionConfig' as needed
 
 with app.app_context():
-    db.drop_all() 
+    # db.drop_all() 
     db.create_all() 
 
 # app.run() #remove when deploying to production server
